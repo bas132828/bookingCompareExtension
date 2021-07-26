68d43534b9dd10988f57cedaf521084cf6393628
Andrei <1328bas@gmail.com>
2021-07-26 08:58:32 +0300

    add to compare button added with basic logic

--- chrome_extension 2/contentScript.js
+++ chrome_extension 2/contentScript.js
@@ -1,12 +1,38 @@
-const city = document.querySelector("#ss");
 
-const buttonSubmit = document.querySelector(
-  "#frm > div.xp__fieldset.js--sb-fieldset.accommodation > div.xp__button > div.sb-searchbox-submit-col.-submit-button > button"
-);
-const buttonClose = document.querySelector(
-  "#frm > div.xp__fieldset.js--sb-fieldset.accommodation > div.xp__input-group.xp__search > div:nth-child(11) > div.c-autocomplete.sb-destination.-with-clear.region_second_line > button"
-);
-buttonClose.click();
-city.value = "London";
+ if(document.querySelectorAll(
+    "#hotellist_inner > div.sr_item.sr_item_new.sr_item_default.sr_property_block"
+  ).length
+) {
+    const nodesOnViewPort = Array.from(
+      document.querySelectorAll(
+        "#hotellist_inner > div.sr_item.sr_item_new.sr_item_default.sr_property_block"
+      )
+    );
+      nodesOnViewPort.forEach((el, i)=> {
+      const html = `
+        <button class='compare-btn btn-${i}}' >Add to compare</button>
+        `
+      el.insertAdjacentHTML("afterbegin", html)
+    })
+    const hotelsPicked = []
+    document.querySelectorAll('.compare-btn').forEach(el=>{
+      el.addEventListener('click', function () {
+        hotelsPicked.push(this.closest('.sr_item'))
+        chrome.storage.sync.set({hotelsPicked});
+        console.log(hotelsPicked)
+        // console.log(chrome.storage.sync.get("hotel"))
+     })
+    })
+      
+  // const arrayToSend = firstFiveNodes.map((el, i) => {
+  //   return {
+  //     title: el.querySelector(".sr-hotel__name").textContent,
+  //     img: el.querySelector("img").getAttribute("src"),
+  //     price: el.querySelector("div.bui-price-display__value").textContent,
+  //     link: `https://booking.com/${el.querySelector("a").getAttribute("href")}`,
+  //     id: i,
+  //   };
+  // });
 
-buttonSubmit.click();
+  // chrome.storage.sync.set({ arrayToSend });
+}
