0dc48b42654b56f43635089ca36bf42d384ad888
Andrei Bodrov <63089699+bas132828@users.noreply.github.com>
2021-07-26 07:52:12 +0300

    Add files via upload

---
+++ chrome_extension 2/contentScript.js
@@ -0,0 +1,12 @@
+const city = document.querySelector("#ss");
+
+const buttonSubmit = document.querySelector(
+  "#frm > div.xp__fieldset.js--sb-fieldset.accommodation > div.xp__button > div.sb-searchbox-submit-col.-submit-button > button"
+);
+const buttonClose = document.querySelector(
+  "#frm > div.xp__fieldset.js--sb-fieldset.accommodation > div.xp__input-group.xp__search > div:nth-child(11) > div.c-autocomplete.sb-destination.-with-clear.region_second_line > button"
+);
+buttonClose.click();
+city.value = "London";
+
+buttonSubmit.click();
---
+++ chrome_extension 2/foreground.js
@@ -0,0 +1,24 @@
+if (
+  document.querySelector(
+    "#hotellist_inner > div.sr_item.sr_item_new.sr_item_default.sr_property_block"
+  )
+) {
+  const nodesOnViewPort = Array.from(
+    document.querySelectorAll(
+      "#hotellist_inner > div.sr_item.sr_item_new.sr_item_default.sr_property_block"
+    )
+  );
+  const firstFiveNodes = nodesOnViewPort.slice(0, 5);
+  console.log(firstFiveNodes);
+  const arrayToSend = firstFiveNodes.map((el, i) => {
+    return {
+      title: el.querySelector(".sr-hotel__name").textContent,
+      img: el.querySelector("img").getAttribute("src"),
+      price: el.querySelector("div.bui-price-display__value").textContent,
+      link: `https://booking.com/${el.querySelector("a").getAttribute("href")}`,
+      id: i,
+    };
+  });
+
+  chrome.storage.sync.set({ arrayToSend });
+}
