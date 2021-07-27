ecaa066a8a7b8a85f39638626cee3155a240c1d9
Andrei <1328bas@gmail.com>
2021-07-27 20:25:17 +0300

    refactored content scripts

--- chrome_extension 2/foreground.js
+++ chrome_extension 2/foreground.js
@@ -1,24 +1,41 @@
-// if (
-//   document.querySelector(
-//     "#hotellist_inner > div.sr_item.sr_item_new.sr_item_default.sr_property_block"
-//   )
-// ) {
-//   const nodesOnViewPort = Array.from(
-//     document.querySelectorAll(
-//       "#hotellist_inner > div.sr_item.sr_item_new.sr_item_default.sr_property_block"
-//     )
-//   );
-//   const firstFiveNodes = nodesOnViewPort.slice(0, 5);
-//   console.log(firstFiveNodes);
-//   const arrayToSend = firstFiveNodes.map((el, i) => {
-//     return {
-//       title: el.querySelector(".sr-hotel__name").textContent,
-//       img: el.querySelector("img").getAttribute("src"),
-//       price: el.querySelector("div.bui-price-display__value").textContent,
-//       link: `https://booking.com/${el.querySelector("a").getAttribute("href")}`,
-//       id: i,
-//     };
-//   });
-
-//   chrome.storage.sync.set({ arrayToSend });
-// }
+if (
+  document.querySelectorAll(
+    "#hotellist_inner > div.sr_item.sr_item_new.sr_item_default.sr_property_block"
+  ).length
+) {
+  const nodesOnViewPort = Array.from(
+    document.querySelectorAll(
+      "#hotellist_inner > div.sr_item.sr_item_new.sr_item_default.sr_property_block"
+    )
+  );
+  nodesOnViewPort.forEach((el, i) => {
+    const html = `
+          <button class='compare-btn btn-${i}}' >Add to compare</button>
+          `;
+    el.querySelector("img").insertAdjacentHTML("afterend", html);
+  });
+  const hotelsPicked = [];
+  document.querySelectorAll(".compare-btn").forEach((el) => {
+    el.addEventListener("click", function (e) {
+      e.preventDefault();
+      hotelsPicked.push(this.closest(".sr_item"));
+      sendingFn();
+    });
+  });
+  function sendingFn() {
+    if (hotelsPicked.length) {
+      const hotelsPickedtoSend = hotelsPicked.map((el) => {
+        return {
+          title: el.querySelector(".sr-hotel__name").textContent,
+          img: el.querySelector("img").getAttribute("src"),
+          price: el.querySelector("div.bui-price-display__value").textContent,
+          link: `https://booking.com/${el
+            .querySelector("a")
+            .getAttribute("href")}`,
+          id: Math.random().toString().slice(2, 8),
+        };
+      });
+      chrome.storage.sync.set({ hotels: hotelsPickedtoSend });
+    }
+  }
+}
