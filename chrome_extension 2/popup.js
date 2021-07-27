a69fd8bd2738c43684417d4d83a9464ff4ed85a0
Andrei <1328bas@gmail.com>
2021-07-27 14:57:48 +0300

    adding and deleting hotels to compare(no storage interaction when deleting)

--- chrome_extension 2/popup.js
+++ chrome_extension 2/popup.js
@@ -1,22 +1,30 @@
+chrome.storage.sync.get(["hotels"], function (result) {
+  if (!result.hotels.length) return;
 
+  result.hotels.forEach((el) => {
+    const html = `
+      <ul class='ce_container container-${el.id}'>
+        <a class='cover-link' href="${el.link}" target="_blank" rel="noopener noreferrer">
+        </a>
+          <div class='drop-shadow'></div>
+          <li class='ce_container--item'> 
+            <button class='delete-btn'>&times;</button>
+            <img class='popup_container--hotel-image' src='${el.img}' alt='hotel-img'>
+              <h3 class='ce_container--item__title'>${el.title}</h3>
+            <span class='popup_container--price'>${el.price}</span>
+          </li>
+      </ul>
+      `;
+    document
+      .querySelector(".popup_container")
+      .insertAdjacentHTML("afterbegin", html);
+  });
 
-// chrome.storage.sync.get(["arrayToSend"], function (result) {
-//   result.arrayToSend.forEach((el) => {
-//     const html = `
-//       <ul class='ce_container'>
-//         <a class='cover-link' href="${el.link}" target="_blank" rel="noopener noreferrer">
-//         </a>
-//           <div class='drop-shadow'></div>
-        
-//           <li class='ce_container--item'> 
-//             <img class='popup_container--hotel-image' src='${el.img}' alt='hotel-img'>
-//               <h3 class='ce_container--item__title'>${el.title}</h3>
-//             <span class='popup_container--price'>${el.price}</span>
-//           </li>
-//       </ul>
-//       `;
-//     document
-//       .querySelector(".popup_container")
-//       .insertAdjacentHTML("afterbegin", html);
-//   });
-// });
+  const closeButtons = document.querySelectorAll(".delete-btn");
+
+  closeButtons.forEach((el) => {
+    el.addEventListener("click", () => {
+      el.closest(".ce_container").remove();
+    });
+  });
+});
